83f3633d04de4d262e1e0f5ef53f2053d55c922d
Khanh Nguyen <khanhng3009@gmail.com>
2022-12-25 18:04:23 +0700

    enable cors 3

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
