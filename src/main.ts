bd8148147473c0c5088baab8eb0fab247a287985
Khanh Nguyen <khanhng3009@gmail.com>
2022-12-25 18:14:08 +0700

    update vercel config

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
