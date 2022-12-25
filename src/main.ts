5601805124916c4e378a8993cad98386f1bd5ec8
Khanh Nguyen <khanhng3009@gmail.com>
2022-12-25 18:23:54 +0700

    update cors and hope

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
